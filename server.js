ce32061d7a6b759d6b3cfca1932d41688d190e80
Avan Singh <avansingh085@gmail.com>
2025-03-03 02:16:38 +0530

    ok

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const { Server } = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: {
-    origin: '*',
-  }
-});
+    cors: {
+      origin: "https://gamezone-avan.netlify.app",
+      methods: ["GET", "POST"]
+    }
+  });
 
 io.on('connection', (socket) => {
   let currentRoom = null;
